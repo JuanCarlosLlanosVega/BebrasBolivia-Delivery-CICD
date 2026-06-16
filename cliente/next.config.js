f9b007fa2569616cd7a2e7a9f67fbe20f3df411e
JuanCarlosLlanosVega <juancarlosllanosvega123@gmail.com>
2026-06-16 02:43:24 -0400

    ci: agregar deploy automatico a firebase hosting

--- cliente/next.config.js
+++ cliente/next.config.js
@@ -1,7 +1,13 @@
+/* eslint-env node */
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  allowedDevOrigins: ['localhost:3000', '26.166.58.21'],
+  allowedDevOrigins: ["localhost:3000", "26.166.58.21"],
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
